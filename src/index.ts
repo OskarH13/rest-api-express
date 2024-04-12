8ce21db04b75cbb1f9a0c6afee302d180afb0598
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-04-12 13:37:13 +0200

    Adding Friday teaching

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express'
+import { infoRouter } from './routes/info'
+import { notesRouter } from './routes/notes'
 
 // we define our server and port
 
 const app = express()
 const port = 3000
+
+// Setup custom middleware
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Setup routes
+
+app.use('/info', infoRouter)
+app.use('/notes', notesRouter)
+
 
 // http-request: method (GET, POST, ...), URL (path)
 // this structure is used by express
@@ -11,13 +24,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hallo Welt! Ich bin ein Express-Server')
 })
 
-app.get('/info', (req: Request, res: Response) => {
-  res.send('GET - Wir haben heute viel über APIs und HTTP gelernt.')
-})
-
-app.post('/info', (req: Request, res: Response) => {
-  res.send('POST - Deine Post Anfrage ist angekommen!')
-})
 
 
 // start our server
