79c435b2410c5b990d9054f1f0b886cd653a58ff
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-04-12 16:52:03 +0200

    Homework Friday fertig

--- src/services/Homework1.ts
+++ src/services/Homework1.ts
@@ -1,4 +1,5 @@
-// Aufgabe 3: Teil 1
+// Aufgabe 3. Teil 1
+
 // services/data.ts
 
 import * as fs from 'fs';
@@ -10,26 +11,11 @@ interface Note {
   user: string;
 }
 
-// Funktion zum Lesen der Notizen aus der Datei
-function readNotes(): Note[] {
+export function readNotes(): Note[] {
   const notesRaw = fs.readFileSync('data/notes.json', 'utf8');
   return JSON.parse(notesRaw);
 }
 
-// Funktion zum Schreiben der Notizen in die Datei
-function writeNotes(notes: Note[]): void {
+export function writeNotes(notes: Note[]): void {
   fs.writeFileSync('data/notes.json', JSON.stringify(notes, null, 2));
-}
-
-// Funktion zum Hinzufügen einer neuen Notiz
-export function addNote(title: string, content: string, user: string): void {
-  const notes = readNotes();
-  const newNote: Note = {
-    id: notes.length + 1, // Neue ID erreichen
-    title,
-    content,
-    user
-  };
-  notes.push(newNote);
-  writeNotes(notes);
 }
